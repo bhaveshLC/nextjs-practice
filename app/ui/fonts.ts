08c9e485146b34d77b44cebec6f74cd382765ede
bhaveshLC <blchaudhari546@gmail.com>
2025-08-21 16:53:05 +0530

    init commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = {
+    className: "font-sans", // Replace with your actual font class if needed
+};
